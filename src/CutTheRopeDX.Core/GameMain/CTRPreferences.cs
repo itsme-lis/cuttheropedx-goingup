ad0e9df7e29600f44cb7abd5c7cbed74006ef26d
It's Me, Lis! <118456898+itsme-lis@users.noreply.github.com>
2026-09-17 23:00:11 +0700

    Change PREFS_SELECTED_OMNOM value from 0 to 1

--- src/CutTheRopeDX.Core/GameMain/CTRPreferences.cs
+++ src/CutTheRopeDX.Core/GameMain/CTRPreferences.cs
@@ -649,7 +649,7 @@ namespace CutTheRopeDX.GameMain
             SetIntForKey(0, "PREFS_SOCKS_USED", true);
             SetIntForKey(0, "PREFS_SELECTED_CANDY", true);
             SetIntForKey(0, "PREFS_SELECTED_ROPE", true);
-            SetIntForKey(0, "PREFS_SELECTED_OMNOM", true);
+            SetIntForKey(1, "PREFS_SELECTED_OMNOM", true);
             SetIntForKey(0, "PREFS_SELECTED_TRACE", true);
             SetBooleanForKey(false, "PREFS_CANDY_WAS_CHANGED", true);
             SetBooleanForKey(true, "PREFS_GAME_CENTER_ENABLED", true);
